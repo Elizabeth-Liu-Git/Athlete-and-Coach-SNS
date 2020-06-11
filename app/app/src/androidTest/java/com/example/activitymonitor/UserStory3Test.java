99dcad74286173d2af88682309699b7a523a74e6
Yuqiao Liu <yq459189@dal.ca>
2020-06-11 14:31:09 -0300

    added some files

---
+++ app/app/src/androidTest/java/com/example/activitymonitor/UserStory3Test.java
@@ -0,0 +1,13 @@
+package com.example.activitymonitor;
+import org.junit.Test;
+
+public class UserStory3Test {
+    @Test
+    public void listOfExericise(){
+
+    }
+    public void testSchedule() {
+
+    }
+    public void testNots(){}
+}
