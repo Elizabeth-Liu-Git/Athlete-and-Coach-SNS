efb595b8193cb32a067c7467e767c04ee415f9ee
Alexander Rockwell <ch939037@dal.ca>
2020-06-10 23:21:31 -0300

    User Story 2/10 Checkpoint: Documentation and refactoring, test refinement

--- app/app/src/main/java/com/example/activitymonitor/startExercise.java
+++ app/app/src/main/java/com/example/activitymonitor/startExercise.java
@@ -7,13 +7,17 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Chronometer;
 
+/**
+ * StartExercise()
+ * Contains the screen which displays the timer and allows an athlete to start a particular exercise and record data
+ */
 public class StartExercise extends AppCompatActivity {
 
-    private Chronometer exerciseChrono; //Chronometer to time activity
-    private boolean chronoRunning;//Boolean that keeps track of whether the chronometer is running
-    private long offSet;//Offset for calculating the amount of time elapsed
-    private boolean doneExercise = false;//Boolean that keeps track of whether the exercise has been completed
+    private Chronometer exerciseChrono; //Chronometer object (stopwatch)
+    private long offSet;//Offset used to calculate duration of the exercise
 
+    protected static boolean currentlyExercising = false;//Boolean that indicates whether the exercise is currently occurring
+    protected static boolean doneExercise = false; //Boolean that indicates whether the exercise is done
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,42 +44,41 @@ public class StartExercise extends AppCompatActivity {
 
     /**
      * startExercise()
-     * Serves to start the exercise and start the exercise timer
-     * @param v Contains the view
-     *
+     * Method used to start the exercise, including the stopwatch
+     * @param v View needed for chronometer
      */
     public void startExercise(View v){
         //When Chronometer not running
-        if(!chronoRunning){
+        if(!currentlyExercising){
             exerciseChrono.setBase(SystemClock.elapsedRealtime() - offSet);
             exerciseChrono.start();
-            chronoRunning=true;
+            currentlyExercising =true;
         }
-
     }
 
     /**
      * finishExercise()
-     * Serves to finish and submit the exercise information to the database
-     * @param v contains View
+     * Method used once exercise is completed
+     * @param v View needed for chronometer
      */
-    public void finishExercise(View v){
-        //TODO Method for button that submits exercise
+    public static void finishExercise(View v){
+        doneExercise=true;
+        //TODO implement functionality to store collected data in FireStore
 
     }
 
     /**
      * pauseExercise()
-     * serves to pause the exercise and pause the timer
-     * @param v contains View
+     * Method used to pause the chronometer and data collection
+     * @param v View needed for chronometer
      */
     public void pauseExercise(View v){
 
-        if(chronoRunning){
+        if(currentlyExercising){
             exerciseChrono.stop();
             offSet = SystemClock.elapsedRealtime()- exerciseChrono.getBase();
-
-            chronoRunning=false;
+            currentlyExercising =false;
         }
+
     }
 }
