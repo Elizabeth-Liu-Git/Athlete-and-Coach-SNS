ac2664c27c2cf8add6cc4644b7986f2ea8819e77
Alexander Rockwell <ch939037@dal.ca>
2020-06-11 23:33:30 -0300

    Added testing for User Story 2

--- app/app/src/androidTest/java/com/example/activitymonitor/StartExerciseTest.java
+++ app/app/src/androidTest/java/com/example/activitymonitor/StartExerciseTest.java
@@ -1,22 +1,48 @@
 package com.example.activitymonitor;
 
-import org.junit.Test;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
+import com.example.activitymonitor.StartExercise;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 public class StartExerciseTest {
+    @Rule
+    public ActivityScenarioRule<StartExercise> scenario1 = new ActivityScenarioRule<>(StartExercise.class);
 
     @Test
     public void startExercise() {
 
-    }
+        onView(withId(R.id.start_btn_startexercise)).perform(click());
 
-    @Test
-    public void finishExercise() {
+        assertTrue(StartExercise.currentlyExercising);
 
     }
 
     @Test
     public void pauseExercise() {
+
+        onView(withId(R.id.pause_btn_startexercise)).perform(click());
+
+        assertFalse(StartExercise.currentlyExercising);
     }
+
+    @Test
+    public void stopExercise() {
+
+        onView(withId(R.id.submit_btn_startexercise)).perform(click());
+
+        assertTrue(StartExercise.doneExercise);
+    }
+
+
 }
