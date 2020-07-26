80616f23c15ba96a4ce2eb1b5a86c4593c2f4a2a
zacharylong <zcz820379@dal.ca>
2020-07-26 17:08:34 -0300

    Removed the calender and now allows for filter by complete or athlete

--- app/src/main/java/com/example/activitymonitor/CoachCalendar.java
+++ app/src/main/java/com/example/activitymonitor/CoachCalendar.java
@@ -4,8 +4,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -33,6 +36,8 @@ public class CoachCalendar extends AppCompatActivity {
     static ArrayList<String> exerciseNameArray = new ArrayList<>();
     static ArrayList<String> athleteNameArray = new ArrayList<>();
     static ArrayList<String> noteArray = new ArrayList<>();
+    static ArrayList<String> athleteSpinnerArray = new ArrayList<>();
+    public String selectedAthlete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +53,26 @@ public class CoachCalendar extends AppCompatActivity {
             }
         });
 
-        calendarView = (CalendarView) findViewById(R.id.calender);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                date = dayOfMonth + "/" + (month+1) + "/" + year;
-                getExercise1();
+        Spinner completedSpinner = (Spinner) findViewById(R.id.spinner);
+        String[] items = new String[] {"Completed Exercises", "Not Completed Exercises"};
+        ArrayAdapter<String> completedAdaptor = new ArrayAdapter<String>(CoachCalendar.this, android.R.layout.simple_spinner_dropdown_item, items);
+        completedAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        completedSpinner.setAdapter(completedAdaptor);
 
+        Spinner athleteSpinner = (Spinner) findViewById(R.id.spinnerAthlete);
+        athleteSpinnerArray.add("Choose an Athlete");
+        getNamesFirebase();
+        ArrayAdapter<String> atheleteAdaptor = new ArrayAdapter<String>(CoachCalendar.this, android.R.layout.simple_spinner_dropdown_item, athleteSpinnerArray);
+        atheleteAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        athleteSpinner.setAdapter(atheleteAdaptor);
+        athleteSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedAthlete = athleteSpinner.getSelectedItem().toString();
+                getExercise();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
 
@@ -69,16 +87,30 @@ public class CoachCalendar extends AppCompatActivity {
             }
         });
     }
+
+    private void getNamesFirebase(){
+        db = FirebaseFirestore.getInstance();
+        db.collection("Assigned Exercise")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                athleteSpinnerArray.add(document.get("Athlete name").toString());
+                            }
+                        }
+                    }
+                });
+    }
+
     /**
      * retrieves assignedExercise activity from firebase and saves to to arrayList
      */
-    private void getExercise1(){
-        exerciseNameArray.clear();
-        athleteNameArray.clear();
-        noteArray.clear();
+    private void getExercise(){
         db = FirebaseFirestore.getInstance();
         db.collection("Assigned Exercise")
-                .whereEqualTo("Date", date)
+                .whereEqualTo("Athlete name", selectedAthlete)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
--- app/src/main/java/com/example/activitymonitor/PastActivity.java
+++ app/src/main/java/com/example/activitymonitor/PastActivity.java
@@ -33,5 +33,8 @@ public class PastActivity extends CoachCalendar {
 
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(mAdaptor);
+        exerciseNameArray.clear();
+        athleteNameArray.clear();
+        noteArray.clear();
     }
 }
